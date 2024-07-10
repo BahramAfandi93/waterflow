ec183e6ac42407458c3f01d1f4b59e170c02a3d4
bafandi <bahram.afandi@pashabank.az>
2024-07-10 17:14:47 +0400

    Checkpoint till DB

--- src/main/kotlin/com/calcpro/flowmaster/dao/repository/EngineerRepository.kt
+++ src/main/kotlin/com/calcpro/flowmaster/dao/repository/EngineerRepository.kt
@@ -1,7 +1,7 @@
 package com.calcpro.flowmaster.dao.repository;
 
-import com.calcpro.flowmaster.dao.entity.User
+import com.calcpro.flowmaster.dao.entity.Engineer
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface EngineerRepository : JpaRepository<User, Long> {
+interface EngineerRepository : JpaRepository<Engineer, Long> {
 }
